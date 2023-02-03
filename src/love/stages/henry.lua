229e7187ba9dce0496551c38633c8e2f45762547
clothing-hanger <kelbymixon1@gmail.com>
2023-02-02 23:05:23 -0600

    stuff

--- src/love/stages/henry.lua
+++ src/love/stages/henry.lua
@@ -1,13 +1,13 @@
 return {
     enter = function()
         stageImages = {
-            graphics.newImage(love.graphics.newImage(graphics.imagePath("henry/stagehenry"))),
+            graphics.newImage(graphics.imagePath("henry/stagehenry")),
         }
 
 
         enemy = love.filesystem.load("sprites/characters/HENRY_ASSS.lua")()
         enemyTwo = enemy
-        boyfriend = Character.boyfriend(0,0)
+        boyfriend = love.filesystem.load("sprites/boyfriend.lua")()
 
 
         girlfriend.x, girlfriend.y = -200, -445
@@ -17,8 +17,26 @@ return {
 
         
 
-        cam.sizeX, cam.sizeY = 0.7, 0.7
-        camScale.x, camScale.y = 0.7, 0.7
+        camera.sizeX, camera.sizeY = 0.7, 0.7
+        camera.scaleX, camera.scaleY = 0.7, 0.7
+
+
+
+
+
+
+
+        enemy = love.filesystem.load("sprites/characters/Reginald_Assets.lua")()
+        enemyTwo = love.filesystem.load("sprites/characters/rhm.lua")()
+
+        camera:addPoint("boyfriend", -boyfriend.x + 100, -boyfriend.y + 75)
+        camera:addPoint("enemy", 500, -boyfriend.y + 75)
+
+        enemy.x, enemy.y = -517, 40
+        enemyTwo.x, enemyTwo.y = -286, 34
+        girlfriend.x, girlfriend.y = -258, -255
+        boyfriend.x, boyfriend.y = 243, 63
+
     end,
 
     load = function()
@@ -27,10 +45,14 @@ return {
             enemy = love.filesystem.load("sprites/characters/Reginald_Assets.lua")()
             enemyTwo = love.filesystem.load("sprites/characters/rhm.lua")()
 
+            camera:addPoint("boyfriend", -boyfriend.x + 100, -boyfriend.y + 75)
+            camera:addPoint("enemy", 500, -boyfriend.y + 75)
+
             enemy.x, enemy.y = -517, 40
             enemyTwo.x, enemyTwo.y = -286, 34
             girlfriend.x, girlfriend.y = -258, -255
             boyfriend.x, boyfriend.y = 243, 63
+            
         elseif song == 3 then
             enemy = love.filesystem.load("sprites/characters/HENRY_ASSS.lua")()
             enemyTwo = love.filesystem.load("sprites/characters/Ellie_Assets.lua")()
@@ -56,13 +78,15 @@ return {
 
     draw = function()
 		love.graphics.push()
-			love.graphics.translate(cam.x, cam.y)
+			love.graphics.translate(camera.x, camera.y)
+			love.graphics.translate(camera.ex, camera.ey)
+
             stageImages[1]:draw()
             girlfriend:draw()
             if song == 3 or song == 4 then
                 enemyTwo:draw()
             end
-            --enemyTwo:draw()
+            enemyTwo:draw()
             enemy:draw()
             
 
