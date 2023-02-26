0e387c86e0750ce6b994a4fd70d4684da6fea069
clothing-hanger <kelbymixon1@gmail.com>
2023-02-26 17:08:05 -0600

    red

---
+++ src/love/sprites/characters/bfghostDEAD.lua
@@ -0,0 +1,144 @@
+return graphics.newSprite(
+	love.graphics.newImage(graphics.imagePath("characters/bfghostDEAD")), -- Do not add the .png extension
+	{
+		{x = 2497, y = 3961, width = 422, height = 369, offsetX = 0, offsetY = 0, offsetWidth = 426, offsetHeight = 497}, -- 1: death0000
+		{x = 2497, y = 3961, width = 422, height = 369, offsetX = 0, offsetY = 0, offsetWidth = 426, offsetHeight = 497}, -- 2: death0001
+		{x = 833, y = 3961, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 3: death0002
+		{x = 833, y = 3961, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 4: death0003
+		{x = 2920, y = 3961, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 5: death0004
+		{x = 2920, y = 3961, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 6: death0005
+		{x = 3335, y = 3961, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 7: death0006
+		{x = 3335, y = 3961, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 8: death0007
+		{x = 1, y = 3963, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 9: death0008
+		{x = 1, y = 3963, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 10: death0009
+		{x = 833, y = 3961, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 11: death0010
+		{x = 833, y = 3961, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 12: death0011
+		{x = 2920, y = 3961, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 13: death0012
+		{x = 2920, y = 3961, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 14: death0013
+		{x = 3335, y = 3961, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 15: death0014
+		{x = 1248, y = 3963, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 16: death0015
+		{x = 2081, y = 3963, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 17: death0016
+		{x = 2081, y = 3963, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 18: death0017
+		{x = 1663, y = 3964, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 19: death0018
+		{x = 1663, y = 3964, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 20: death0019
+		{x = 833, y = 4329, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 21: death0020
+		{x = 833, y = 4329, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 22: death0021
+		{x = 2920, y = 4329, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 23: death0022
+		{x = 2920, y = 4329, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 24: death0023
+		{x = 3335, y = 4329, width = 414, height = 367, offsetX = -12, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 25: death0024
+		{x = 2913, y = 1023, width = 419, height = 492, offsetX = -7, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 26: death0025
+		{x = 3333, y = 1023, width = 419, height = 492, offsetX = -7, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 27: death0026
+		{x = 833, y = 1027, width = 417, height = 487, offsetX = -9, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 28: death0027
+		{x = 1, y = 1030, width = 417, height = 487, offsetX = -9, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 29: death0028
+		{x = 419, y = 1515, width = 415, height = 489, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 30: death0029
+		{x = 835, y = 1515, width = 415, height = 489, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 31: death0030
+		{x = 2913, y = 1516, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 32: death0031
+		{x = 3329, y = 1516, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 33: death0032
+		{x = 3329, y = 1516, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 34: death0033
+		{x = 1, y = 1518, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 35: death0034
+		{x = 1251, y = 1518, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 36: death0035
+		{x = 2497, y = 1518, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 37: death0036
+		{x = 1667, y = 1519, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 38: death0037
+		{x = 417, y = 2005, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 39: death0038
+		{x = 833, y = 2005, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 40: death0039
+		{x = 2913, y = 2005, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 41: death0040
+		{x = 3329, y = 2005, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 42: death0041
+		{x = 1, y = 2007, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 43: death0042
+		{x = 1249, y = 2007, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 44: death0043
+		{x = 2083, y = 2007, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 45: death0044
+		{x = 1665, y = 2008, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 46: death0045
+		{x = 417, y = 2494, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 47: death0046
+		{x = 833, y = 2494, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 48: death0047
+		{x = 2499, y = 2494, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 49: death0048
+		{x = 2915, y = 2494, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 50: death0049
+		{x = 3331, y = 2494, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 51: death0050
+		{x = 1, y = 2496, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 52: death0051
+		{x = 1249, y = 2496, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 53: death0052
+		{x = 2081, y = 2496, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 54: death0053
+		{x = 1665, y = 2497, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 55: death0054
+		{x = 417, y = 2983, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 56: death0055
+		{x = 833, y = 2983, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 57: death0056
+		{x = 2497, y = 2983, width = 415, height = 488, offsetX = -11, offsetY = -5, offsetWidth = 426, offsetHeight = 497}, -- 58: death0057
+
+		{x = 417, y = 3961, width = 415, height = 488, offsetX = 0, offsetY = -44, offsetWidth = 415, offsetHeight = 532}, -- 59: confirm death0000
+		{x = 1, y = 1, width = 415, height = 532, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 60: confirm death0001
+		{x = 417, y = 1, width = 415, height = 529, offsetX = 0, offsetY = -3, offsetWidth = 415, offsetHeight = 532}, -- 61: confirm death0002
+		{x = 833, y = 1, width = 415, height = 526, offsetX = 0, offsetY = -6, offsetWidth = 415, offsetHeight = 532}, -- 62: confirm death0003
+		{x = 1249, y = 1, width = 415, height = 524, offsetX = 0, offsetY = -8, offsetWidth = 415, offsetHeight = 532}, -- 63: confirm death0004
+		{x = 1665, y = 1, width = 415, height = 521, offsetX = 0, offsetY = -11, offsetWidth = 415, offsetHeight = 532}, -- 64: confirm death0005
+		{x = 2081, y = 1, width = 415, height = 518, offsetX = 0, offsetY = -14, offsetWidth = 415, offsetHeight = 532}, -- 65: confirm death0006
+		{x = 2497, y = 1, width = 415, height = 516, offsetX = 0, offsetY = -16, offsetWidth = 415, offsetHeight = 532}, -- 66: confirm death0007
+		{x = 2913, y = 1, width = 415, height = 513, offsetX = 0, offsetY = -19, offsetWidth = 415, offsetHeight = 532}, -- 67: confirm death0008
+		{x = 3329, y = 1, width = 415, height = 511, offsetX = 0, offsetY = -21, offsetWidth = 415, offsetHeight = 532}, -- 68: confirm death0009
+		{x = 3329, y = 513, width = 415, height = 509, offsetX = 0, offsetY = -23, offsetWidth = 415, offsetHeight = 532}, -- 69: confirm death0010
+		{x = 2913, y = 515, width = 415, height = 507, offsetX = 0, offsetY = -25, offsetWidth = 415, offsetHeight = 532}, -- 70: confirm death0011
+		{x = 2497, y = 518, width = 415, height = 505, offsetX = 0, offsetY = -27, offsetWidth = 415, offsetHeight = 532}, -- 71: confirm death0012
+		{x = 2081, y = 520, width = 415, height = 503, offsetX = 0, offsetY = -29, offsetWidth = 415, offsetHeight = 532}, -- 72: confirm death0013
+		{x = 1665, y = 523, width = 415, height = 501, offsetX = 0, offsetY = -31, offsetWidth = 415, offsetHeight = 532}, -- 73: confirm death0014
+		{x = 1249, y = 526, width = 415, height = 500, offsetX = 0, offsetY = -32, offsetWidth = 415, offsetHeight = 532}, -- 74: confirm death0015
+		{x = 833, y = 528, width = 415, height = 498, offsetX = 0, offsetY = -34, offsetWidth = 415, offsetHeight = 532}, -- 75: confirm death0016
+		{x = 417, y = 531, width = 415, height = 497, offsetX = 0, offsetY = -35, offsetWidth = 415, offsetHeight = 532}, -- 76: confirm death0017
+		{x = 1, y = 534, width = 415, height = 495, offsetX = 0, offsetY = -37, offsetWidth = 415, offsetHeight = 532}, -- 77: confirm death0018
+		{x = 2081, y = 1024, width = 415, height = 494, offsetX = 0, offsetY = -38, offsetWidth = 415, offsetHeight = 532}, -- 78: confirm death0019
+		{x = 2497, y = 1024, width = 415, height = 493, offsetX = 0, offsetY = -39, offsetWidth = 415, offsetHeight = 532}, -- 79: confirm death0020
+		{x = 1665, y = 1025, width = 415, height = 492, offsetX = 0, offsetY = -40, offsetWidth = 415, offsetHeight = 532}, -- 80: confirm death0021
+		{x = 1665, y = 1025, width = 415, height = 492, offsetX = 0, offsetY = -40, offsetWidth = 415, offsetHeight = 532}, -- 81: confirm death0022
+		{x = 1665, y = 1025, width = 415, height = 492, offsetX = 0, offsetY = -40, offsetWidth = 415, offsetHeight = 532}, -- 82: confirm death0023
+		{x = 1665, y = 1025, width = 415, height = 492, offsetX = 0, offsetY = -40, offsetWidth = 415, offsetHeight = 532}, -- 83: confirm death0024
+		{x = 1665, y = 1025, width = 415, height = 492, offsetX = 0, offsetY = -40, offsetWidth = 415, offsetHeight = 532}, -- 84: confirm death0025
+		{x = 1665, y = 1025, width = 415, height = 492, offsetX = 0, offsetY = -40, offsetWidth = 415, offsetHeight = 532}, -- 85: confirm death0026
+		{x = 1665, y = 1025, width = 415, height = 492, offsetX = 0, offsetY = -40, offsetWidth = 415, offsetHeight = 532}, -- 86: confirm death0027
+		{x = 1665, y = 1025, width = 415, height = 492, offsetX = 0, offsetY = -40, offsetWidth = 415, offsetHeight = 532}, -- 87: confirm death0028
+		{x = 1665, y = 1025, width = 415, height = 492, offsetX = 0, offsetY = -40, offsetWidth = 415, offsetHeight = 532}, -- 88: confirm death0029
+		{x = 1665, y = 1025, width = 415, height = 492, offsetX = 0, offsetY = -40, offsetWidth = 415, offsetHeight = 532}, -- 89: confirm death0030
+		{x = 1665, y = 1025, width = 415, height = 492, offsetX = 0, offsetY = -40, offsetWidth = 415, offsetHeight = 532}, -- 90: confirm death0031
+		{x = 1665, y = 1025, width = 415, height = 492, offsetX = 0, offsetY = -40, offsetWidth = 415, offsetHeight = 532}, -- 91: confirm death0032
+		{x = 1665, y = 1025, width = 415, height = 492, offsetX = 0, offsetY = -40, offsetWidth = 415, offsetHeight = 532}, -- 92: confirm death0033
+
+		{x = 2913, y = 2983, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 93: loop death0000
+		{x = 2913, y = 2983, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 94: loop death0001
+		{x = 3329, y = 2983, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 95: loop death0002
+		{x = 3329, y = 2983, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 96: loop death0003
+		{x = 1, y = 2985, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 97: loop death0004
+		{x = 1, y = 2985, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 98: loop death0005
+		{x = 1249, y = 2985, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 99: loop death0006
+		{x = 2081, y = 2985, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 100: loop death0007
+		{x = 1665, y = 2986, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 101: loop death0008
+		{x = 1665, y = 2986, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 102: loop death0009
+		{x = 417, y = 3472, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 103: loop death0010
+		{x = 417, y = 3472, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 104: loop death0011
+		{x = 833, y = 3472, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 105: loop death0012
+		{x = 833, y = 3472, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 106: loop death0013
+		{x = 2497, y = 3472, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 107: loop death0014
+		{x = 2497, y = 3472, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 108: loop death0015
+		{x = 2913, y = 3472, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 109: loop death0016
+		{x = 2913, y = 2983, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 110: loop death0017
+		{x = 3329, y = 3472, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 111: loop death0018
+		{x = 3329, y = 2983, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 112: loop death0019
+		{x = 1, y = 3474, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 113: loop death0020
+		{x = 1, y = 2985, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 114: loop death0021
+		{x = 1249, y = 3474, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 115: loop death0022
+		{x = 1249, y = 2985, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 116: loop death0023
+		{x = 2081, y = 3474, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 117: loop death0024
+		{x = 1665, y = 2986, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 118: loop death0025
+		{x = 1665, y = 3475, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 119: loop death0026
+		{x = 417, y = 3472, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 120: loop death0027
+		{x = 833, y = 3472, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 121: loop death0028
+		{x = 833, y = 3472, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 122: loop death0029
+		{x = 2497, y = 3472, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 123: loop death0030
+		{x = 2497, y = 3472, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 124: loop death0031
+		{x = 2913, y = 3472, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0}, -- 125: loop death0032
+		{x = 2913, y = 3472, width = 415, height = 488, offsetX = 0, offsetY = 0, offsetWidth = 0, offsetHeight = 0} -- 126: loop death0033
+	},
+	{
+		--To get the animations, look at the number and the name of the frame
+		--Start is the first frame, stop is the last frame, speed is the speed of the animation, offsetX and offsetY are the offset of the animation
+		--Get the offset by using the ingame offset editor and pressing 7
+		["dies"] = {start = 1, stop = 58, speed = 24, offsetX = 0, offsetY = 0},
+		["dead confirm"] = {start = 59, stop = 92, speed = 24, offsetX = 0, offsetY = 0},
+		["dead loop"] = {start = 93, stop = 126, speed = 24, offsetX = 0, offsetY = 0},
+
+	},
+	"dies", -- set to default animation
+	false -- If the sprite repeats
+)
---
+++ src/love/sprites/polus/boppers_meltdown.lua
@@ -0,0 +1,28 @@
+return graphics.newSprite(
+	love.graphics.newImage(graphics.imagePath("polus/boppers_meltdown")), -- Do not add the .png extension
+	{
+		{x = 13, y = 13, width = 2967, height = 885, offsetX = 0, offsetY = -28, offsetWidth = 2967, offsetHeight = 913}, -- 1: BoppersMeltdown0000
+		{x = 2993, y = 13, width = 2967, height = 885, offsetX = 0, offsetY = -28, offsetWidth = 2967, offsetHeight = 913}, -- 2: BoppersMeltdown0001
+		{x = 13, y = 911, width = 2963, height = 882, offsetX = -3, offsetY = -28, offsetWidth = 2967, offsetHeight = 913}, -- 3: BoppersMeltdown0002
+		{x = 2989, y = 911, width = 2963, height = 882, offsetX = -3, offsetY = -28, offsetWidth = 2967, offsetHeight = 913}, -- 4: BoppersMeltdown0003
+		{x = 13, y = 1806, width = 2958, height = 873, offsetX = -5, offsetY = -16, offsetWidth = 2967, offsetHeight = 913}, -- 5: BoppersMeltdown0004
+		{x = 2984, y = 1806, width = 2958, height = 873, offsetX = -5, offsetY = -16, offsetWidth = 2967, offsetHeight = 913}, -- 6: BoppersMeltdown0005
+		{x = 13, y = 2692, width = 2951, height = 884, offsetX = -9, offsetY = -1, offsetWidth = 2967, offsetHeight = 913}, -- 7: BoppersMeltdown0006
+		{x = 2977, y = 2692, width = 2951, height = 884, offsetX = -9, offsetY = -1, offsetWidth = 2967, offsetHeight = 913}, -- 8: BoppersMeltdown0007
+		{x = 13, y = 3589, width = 2954, height = 885, offsetX = -8, offsetY = 0, offsetWidth = 2967, offsetHeight = 913}, -- 9: BoppersMeltdown0008
+		{x = 2980, y = 3589, width = 2954, height = 885, offsetX = -8, offsetY = 0, offsetWidth = 2967, offsetHeight = 913}, -- 10: BoppersMeltdown0009
+		{x = 13, y = 4487, width = 2954, height = 885, offsetX = -8, offsetY = 0, offsetWidth = 2967, offsetHeight = 913}, -- 11: BoppersMeltdown0010
+		{x = 2980, y = 4487, width = 2954, height = 885, offsetX = -8, offsetY = 0, offsetWidth = 2967, offsetHeight = 913}, -- 12: BoppersMeltdown0011
+		{x = 13, y = 5385, width = 2954, height = 885, offsetX = -8, offsetY = 0, offsetWidth = 2967, offsetHeight = 913}, -- 13: BoppersMeltdown0012
+		{x = 2980, y = 5385, width = 2954, height = 885, offsetX = -8, offsetY = 0, offsetWidth = 2967, offsetHeight = 913}, -- 14: BoppersMeltdown0013
+		{x = 13, y = 6283, width = 2954, height = 885, offsetX = -8, offsetY = 0, offsetWidth = 2967, offsetHeight = 913} -- 15: BoppersMeltdown0014
+	},
+	{
+		--To get the animations, look at the number and the name of the frame
+		--Start is the first frame, stop is the last frame, speed is the speed of the animation, offsetX and offsetY are the offset of the animation
+		--Get the offset by using the ingame offset editor and pressing 7
+		["anim"] = {start = 1, stop = 15, speed = 24, offsetX = 0, offsetY = 0},
+	},
+	"anim", -- set to default animation
+	true -- If the sprite repeats
+)
